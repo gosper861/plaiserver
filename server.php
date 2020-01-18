ed4eb56fd70c0acb3702a150eb2578ba9a335454
gosper861 <gosper861@icloud.com>
2020-01-18 02:07:38 -0400

    plaiserver
    plaiserver

---
+++ server.php
@@ -0,0 +1,9 @@
+<?php
+$var1 = '<audio id="jp_audio_rcast" preload="none" src="';
+
+include("server/headplayer.txt");
+echo "var streamrcast = { mp3: '" . htmlspecialchars($_GET["streaming"]) . '';
+include("server/1player.txt");
+echo 'mp3:"' . htmlspecialchars($_GET["streaming"]) . '';
+include("server/2player.txt");
+?>
